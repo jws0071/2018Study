e92c1cc0c3cb75def8011fca82d65396c2f62f0f
jws0071 <jws0071@naver.com>
2018-03-04 21:57:07 +0900

    no message

---
+++ 2018Study_project/src/study01.java
@@ -0,0 +1,31 @@
+public class study01 {
+
+	public static void main(String[] args) {
+		
+		int[] num = null;
+		int y = 0;
+		for (int i=1000; i <= 9999; i++) {
+			
+			//num[y]=i;
+			//y++;
+			//System.out.println(num[y]);
+			System.out.println(i);
+		}
+		/*
+				
+		String bin = Integer.toBinaryString(a); 
+		String oct = Integer.toOctalString(a); 
+		String hex = Integer.toHexString(a); 
+		System.out.println("10진수 -> 2진수 : " + bin); 
+		System.out.println("10진수 -> 8진수 : " + oct); 
+		System.out.println("10진수 -> 16진수: " + hex); 
+		System.out.println(); 
+		System.out.println("이진수 -> 10진수 : " + Integer.valueOf(bin, 2)); 
+		System.out.println("8진수 -> 10진수 : " + Integer.valueOf(oct, 8)); 
+		System.out.println("16진수-> 10진수 : " + Integer.valueOf(hex, 16));
+		*/	
+	}
+	
+
+	
+}
