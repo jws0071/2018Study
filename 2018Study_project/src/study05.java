14742434778845afa0eb85124f73a79bd212a381
DESKTOP-H8H11FS\woosun <jws0071@gmail.com>
2018-03-16 13:51:28 +0900

    두개 피벗은 미완성 / 5번 6번 문제는 해결

--- 2018Study_project/src/study05.java
+++ 2018Study_project/src/study05.java
@@ -40,22 +40,12 @@
 			sc.close();
 
 				study05 quick = new study05();
-	      quick.sort(arr_num, 0, arr_num.length - 1);
+	      quick.sort(arr_num, 0, arr_num.length-1);
 
 				
 
 	      BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));  // 속도 차이남 
-				
-	      for(int y=0;y<arr_num.length; y++) {
-	      	//bw.write(Integer.toString(arr_num[y])+"\n" );
-	      	if(arr_num[y] == score) {
-	      		//bw.write(Integer.toString(result)+"\n" );
-	      	}else {
-	      		result ++;
-	      	}
-					
-					
-				}
+	      bw.write(Integer.toString(arr_num[score-1])+"\n" );
 
 	      bw.close();
 			
@@ -70,57 +60,41 @@
 		
 		public void sort(int[] arr, int left, int right) {
 			 	int lo = left + 1;
-	      int gt = right - 1;
+	      
 	      int lt = arr[left];
-	      int ht = arr[right];
+	      
 	      study05 swap_f = new study05();
 	      study05 qu = new study05();
 
-	      
-	      System.out.println(arr[lo]);
-	      System.out.println(lt);
-	      printArray(arr);
+
 	      
 	       while (arr[lo] <= lt ) {
+	      	 lo++;
+      		 if(arr.length == lo) { 
+      			 break;
+      			 }
+	      		
 
-	      		 lo++;
-	      		 if(arr.length == lo) { 
-	      			 --lo; 
-	      			 break;
-	      			 }
           }
-		      System.out.println("1="+arr[lo]);
-		      System.out.println("2="+lt);
-	      
-          while (ht <= arr[gt]) {
+	       swap_f.swapl(arr, left, lo);
+	       printArray(arr);
+	       
+	       int ht = arr[right];
+	       int gt = right - 1;
+
+	       while (ht <= arr[gt]) {
           	gt--;
+          	
           }
-          //System.out.println(gt);
-          //printArray(arr);
+	       if(arr[gt]>= ht) {
+	      	swap_f.swapr(arr, right, gt); 
+	       }
+        	
+        //100 20 30 10 -10 0 1 3 2 5
           
-          
-          if (arr[lo] > lt) {
-          	lo = lo - 1;
-          	swap_f.swap(arr, left, lo);
-	        }else {
-	        	swap_f.swap(arr, left, lo);
-	        	
-	        }
-          //System.out.println(arr[gt]);
-         // System.out.println(ht);
-          //printArray(arr);
-          if(arr[gt] < ht) {
-	        	gt = gt + 1;
-	        	swap_f.swap(arr, right, gt);
-	        	
-	        }else{
-	        	swap_f.swap(arr, right, gt);
-	        }
-	        System.out.println("Start");
-	        printArray(arr);
+         
+ 	        lo = lo - 1;
 	        qu.quick(arr, left, lo-1);
-	        qu.quick(arr, lo+1, gt-1);
-	        qu.quick(arr, gt+1, right);
 
 	        
 	       
@@ -164,35 +138,146 @@
 	            }
 	            
 	        }
-	        printArray(arr);
-	        
-	        
-	        /*
-	        
-	        sort(arr, left, highIdx - 1);
-	        sort(arr, highIdx + 1, right);
-	        
-	        quick(arr, left, lowIdx-1);
-	        quick(arr, lowIdx+1, highIdx-1);
-	        quick(arr, highIdx+1, right);
 
-	        */
-	        System.out.println("left" +left);
-	        System.out.println("right" +right);
-	        sort(arr, left, right);
+	        
+
+	        //sort(arr, left, right);
 	        
 
 	    }
 
-		public void swap(int[] arr, int pi, int Idx) {
-    int temp = arr[Idx];
-    arr[Idx] = arr[pi];
-    arr[pi] = temp;
-    }
+		public void swapl(int[] arr, int pi, int Idx) {
+    int temp = arr[pi];
+    
+    
+	    for(int i=0; i<=Idx-1;i++) {
+	    	if (i == Idx-1) {
+	    		arr[Idx-1] = temp;
+	    		
+	    	}else {
+	    		arr[i] = arr[i+1];
+	    	}
+	    }
+		}
 
-      
+  
+		public void swapr(int[] arr, int pi, int Idx) {
+	    int temp = arr[pi];
+    	
+    	for(int x=pi;x >= Idx;x--) {
+    	
+   
+   
+    		if (x == Idx) {
+	    		arr[x] = temp;
+	    		
+	    	}else {
+	    		arr[x] = arr[x-1];
+	    	}
 
+
+    	}
+
+		}
+
+     
+/*
+		public static void main(String[] args)throws Exception {
 			
+		
+
+				BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
+				StringTokenizer st = null;
+				StringTokenizer st_1 = null;
+				
+				int[] st_arr = new int[2];
+				
+				st = new StringTokenizer(sc.readLine());
+				
+				for(int x=0; x<=1; x++) {
+					st_arr[x] = Integer.parseInt(st.nextToken());
+				}
+				
+				int num = st_arr[0];
+				int score = st_arr[1];
+				int result = 1;
+
+				
+				int[] arr_num = new int[num];
+					
+				st_1 = new StringTokenizer(sc.readLine());
+				for (int i=0; i<arr_num.length; i++) {
+
+					arr_num[i] = Integer.parseInt(st_1.nextToken());
+					
+				}
+
+				sc.close();
+
+					study05 quick = new study05();
+		      quick.sort(arr_num, 0, arr_num.length - 1);
+
+
+	    	      BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));  // 속도 차이남 
+	    	      bw.write(Integer.toString(arr_num[score-1])+"\n" );
+	    	      
+
+			bw.close();
+			
+		}
+	    public void printArray(int[] arr) {
+	        for (int val : arr) {
+	            System.out.print(val + " ");
+	        }
+	        System.out.println("");
+	    }
+
+		
+		public void sort(int[] arr, int left, int right) {
+			 
+	         if (left >= right) {
+	        	 
+	            return;
+	        }
+
+	 
+	        int lowIdx = left + 1;
+	        int highIdx = right;
+	        int pivot = arr[left];
+	 
+
+	        //printArray(arr);
+	        while (lowIdx <= highIdx) {
+	        
+	            while (lowIdx <= right && arr[lowIdx] <= pivot) {
+
+	                lowIdx++;
+	            }
+	 
+	            while (left + 1 <= highIdx && pivot <= arr[highIdx]) {
+	          
+	                highIdx--;
+	            }
+
+	 
+	            if (lowIdx <= highIdx) {
+	                int temp = arr[lowIdx];
+	                arr[lowIdx] = arr[highIdx];
+	                arr[highIdx] = temp;
+	            } else {
+
+	                arr[left] = arr[highIdx]; // pivot이 자신의 자리를 찾아가는 과정
+	                arr[highIdx] = pivot;
+	            }
+	            
+	        }
+	    	    	
+	 
+	        sort(arr, left, highIdx - 1);
+	        sort(arr, highIdx + 1, right);
+	    }
+
+		 */		
 			
 			
 }
--- 2018Study_project/src/study06.java
+++ 2018Study_project/src/study06.java
@@ -1,4 +1,122 @@
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+
 public class study06 {
+	
+
+	public static void main(String[] args)throws Exception {
+
+		BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
+
+		int num = Integer.parseInt(sc.readLine());
+		
+		int[] arr_num = new int[num];
+
+		for (int i=0; i<arr_num.length; i++) {
+
+			arr_num[i] = Integer.parseInt(sc.readLine());
+			
+		}
+		sc.close();
+
+		study06 quick = new study06();
+            quick.sort(arr_num, 0, arr_num.length - 1);
+
+           BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));  // 속도 차이남 
+			
+            for(int y=0;y<arr_num.length; y++) {
+				bw.write(Integer.toString(arr_num[y])+"\n" );
+				
+			}
+
+		bw.close();
+		
+	}
+    public void printArray(int[] arr) {
+        for (int val : arr) {
+            System.out.print(val + " ");
+        }
+        System.out.println("");
+    }
+
+	
+	public void sort(int[] arr, int left, int right) {
+		 
+         if (left >= right) {
+        	 
+            return;
+        }
+
+ 
+        int lowIdx = left + 1;
+        int highIdx = right;
+        int pivot = arr[left];
+ 
+
+     
+        while (lowIdx <= highIdx) {
+        
+            while (lowIdx <= right && arr[lowIdx] <= pivot) {
+            	
+            	
+                lowIdx++;
+            }
+ 
+            while (left + 1 <= highIdx && pivot <= arr[highIdx]) {
+
+                highIdx--;
+            }
+
+            if (lowIdx <= highIdx) {
+                int temp = arr[lowIdx];
+                arr[lowIdx] = arr[highIdx];
+                arr[highIdx] = temp;
+            } else {
+
+
+            	
+                arr[left] = arr[highIdx]; // pivot이 자신의 자리를 찾아가는 과정
+                arr[highIdx] = pivot;
+            }
+            
+        }
+
+ 
+        sort(arr, left, highIdx - 1);
+        sort(arr, highIdx + 1, right);
+    }
+
+
+	
 
 }
+
+
+/*
+ 문제
+N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 수의 개수 N(1<=N<=1,000,000)이 주어진다. 둘째 줄부터 N개의 줄에는 숫자가 주어진다. 이 수는 절대값이 1,000,000보다 작거나 같은 정수이다. 수는 중복되지 않는다.
+
+출력
+첫째 줄부터 N개의 줄에 오름차순으로 정렬한 결과를 한 줄에 하나씩 출력한다.
+
+예제 입력 
+5
+5
+4
+3
+2
+1
+예제 출력 
+1
+2
+3
+4
+5
+*/
