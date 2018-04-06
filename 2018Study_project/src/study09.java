059b34645579469525ca5b5ca082195744ad5c19
DESKTOP-H8H11FS\woosun <jws0071@gmail.com>
2018-04-06 09:32:55 +0900

    git 정리 9번 문제

--- 2018Study_project/src/study09.java
+++ 2018Study_project/src/study09.java
@@ -27,32 +27,7 @@ public class study09 {
 			
 		}
 		sc.close();
-<<<<<<< HEAD
-		
-		for(int i =1; i<=cnt; i++) {
-			st_arr_result[i] = st_arr[i];
-			for(int j=1; j<i;j++) {
-				if(st_arr[j] < st_arr[i] && st_arr_result[i] < st_arr_result[j]+st_arr[i]) {
-					st_arr_result[i] = st_arr_result[j] + st_arr[i];
-					
-				}
-				
-			}
-			if(temp<st_arr_result[i]) {
-				temp = st_arr_result[i];
-			}
-				
-			
-			
-		}
 
-		System.out.println(temp+st_arr[0]);	
-				
-			
-		//Arrays.sort(st_arr_result);
-
-		//System.out.println(st_arr_result[st_arr_result.length-1]);
-=======
  
         for (int i = 0; i < cnt; i++) {
             st_arr_result[i] = st_arr[i];
@@ -77,7 +52,7 @@ public class study09 {
         }
 
 		System.out.println(max);
->>>>>>> 210d86f020b42afa2c847ad122a5da922a0804e8
+
 		
 	}
 	
