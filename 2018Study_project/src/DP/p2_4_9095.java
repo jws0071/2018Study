1f7e7ce427b7df9d19e786c43bab7a6945584aba
jws0071 <jws0071@naver.com>
2018-05-26 21:33:17 +0900

    4번문제

---
+++ 2018Study_project/src/DP/p2_4_9095.java
@@ -0,0 +1,29 @@
+package DP;
+
+import java.util.Scanner;
+
+public class p2_4_9095 {
+	static int[] d = new int[1000+1];
+	public static void Main(String args[]) {
+		 Scanner scan = new Scanner(System.in);
+		    int n = scan.nextInt();
+		    System.out.println(f(n));
+		    scan.close();
+		
+	}
+	public static int f(int n) {
+	    if(n<3) {
+	    	d[n-1] = 2;
+	    	d[n-2] = 1;
+	    	return d[n];
+	    }
+	    if(d[n]>0) {  // 한번 계산해서 값이 있으면 그걸 이용려고
+	        return d[n];
+	    }else {
+	        d[n] = f(n-1) + f(n-2) + f(n-3); //2부터 3 = 2+1, 4 = 3+2,  5 = 5+3 자기 자리에서  앞에 두개의 값을 합치면 자기것이 나옴
+	        return d[n];
+	    }
+	}
+	
+	
+}
